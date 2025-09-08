9b525da7cef99a81e6073519487fd44d3035e460
abdurahmon <250055@newuu.uz>
2025-09-08 19:04:31 +0500

    problems 1-10

---
+++ main.cpp
@@ -0,0 +1,116 @@
+#include <iostream>
+using namespace std;
+// problem 1
+// int main() {
+//     int intNumber = 24;
+//     float floatNumber = 3.14;
+//     double doubleNumber = 45.1234;
+//     bool boolean = true;
+//     char charName = 'A';
+//     cout <<"value of integer is: "<<intNumber<<". Size of it is "<< sizeof(intNumber)<< endl;
+//     cout <<"Value of integer is: "<<doubleNumber<<". Size of it is "<< sizeof(doubleNumber)<< endl;
+//     cout <<"Value of integer is: "<<boolean<<". Size of it is "<< sizeof(boolean)<<endl;
+//     cout <<"Value of integer is: "<<charName<<". Size of it is "<< sizeof(charName)<<endl;
+// }
+
+//problem 2
+// int main() {
+//     string name = "Abduraxmon Abduazizov";
+//     int id = 250055;
+//     string favSong = "Queen - The Show Must Go On";
+//     string favMovie = "Titanic";
+//     cout<<name<<endl;
+//     cout<<id<<endl;
+//     cout<<favSong<<endl;
+//     cout<<favMovie<<endl;
+//
+// }
+
+//problem 3
+// int main() {
+//     string poem = "\"I'm standing\" on the edge of some crazy cliff.\n"
+//                   "What I have to do, I have to catch evrybody\\\n"
+//                   "if they start to go over the cliff-I mean\\\n"
+//                   "\\if they're running and they don't look\n"
+//                   "where they're going I have to come out\n"
+//                   "from somewhere and \t catch them.";
+//     cout << poem << endl;
+// }
+
+// problem 4
+// int main() {
+//     int a = 189;
+//     char b = 'B';
+//     float floatNumber = a/3;
+//     int decimalValue = b;
+//     float floatNumber2 = (a+b)/5;
+//     char c = b + a/10 - 1;
+//     cout<<floatNumber<<"\t"<<decimalValue<<"\t"<<floatNumber2<<"\t"<<c<<endl;
+// }
+
+// problem 5
+// int main() {
+//     int sum = 12 + 38;
+//     int prod = 12 * 38;
+//     cout <<" Sum = "<<sum << endl;
+//     cout <<" Prod = "<<prod << endl;
+// }
+
+// problem 6
+// int main() {
+//     int num1, num2;
+//     cout << "Enter first number: ";
+//     cin >> num1;
+//     cout << "Enter second number: ";
+//     cin >> num2;
+//     int result = num1 + num2;
+//     cout<< "Sum: "<<num1<< " + " <<num2<< " = " << result;;
+// }
+
+// problem 7
+// int main() {
+//     double x, y;
+//     cout<<"Enter x: ";
+//     cin>>x;
+//     cout<<"Enter y: ";
+//     cin>>y;
+//     double result = 3 * (x*x + 3)/y/4 + 3;
+//     cout<<"t = "<<result<<endl;
+// }
+//
+//problem8
+// int main() {
+//     double d;
+//     cout<<"Enter a number: ";
+//     cin>>d;
+//     double r = d/2;
+//     double area = r*r*3.14*4;
+//     cout<<"Area is: "<<area<<endl;
+// }
+
+//problem 9
+// int main() {
+//     double v1, v0, t;
+//     cout << "Please enter the starting velocity (m/s): ";
+//     cin >> v0;
+//     cout << "Please enter the ending velocity(m/s): ";
+//     cin >> v1;
+//     cout << "Please enter the time span(s): ";
+//     cin >> t;
+//     double a = (v1 - v0) / t;
+//     cout<<"Average accleration is: " << a << endl;
+//
+// }
+
+// problem 10
+// int main() {
+//     float s, fE, p;
+//     cout<<"Enter the the distance to drive: ";
+//     cin>>s;
+//     cout<<"Enter the fuel efficiency of the car in miles per gallon: ";
+//     cin>>fE;
+//     cout<<"Enter the price per gallon: ";
+//     cin>>p;
+//     float cost = s/fE * p;
+//     cout<<"The cost is: "<<cost<<endl;
+// }
