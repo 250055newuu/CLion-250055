b70ac38a5d3cc582b284184b83243bdca943b4fe
250055 <250055@newuu.uz>
2025-09-08 13:50:05 +0500

    problems 1-5

--- main.cpp
+++ main.cpp
@@ -1,15 +1,63 @@
 #include <iostream>
 using namespace std;
+// problem 1
+// int main() {
+//     int intNumber = 24;
+//     float floatNumber = 3.14;
+//     double doubleNumber = 45.1234;
+//     bool boolean = true;
+//     char charName = 'A';
+//     cout <<"value of integer is: "<<intNumber<<". Size of it is "<< sizeof(intNumber)<< endl;
+//     cout <<"Value of integer is: "<<doubleNumber<<". Size of it is "<< sizeof(doubleNumber)<< endl;
+//     cout <<"Value of integer is: "<<boolean<<". Size of it is "<< sizeof(boolean)<<endl;
+//     cout <<"Value of integer is: "<<charName<<". Size of it is "<< sizeof(charName)<<endl;
+// }
 
+
+//problem 2
+// int main() {
+//     string name = "Abduraxmon Abduazizov";
+//     int id = 250055;
+//     string favSong = "Queen - The Show Must Go On";
+//     string favMovie = "Titanic";
+//     cout<<name<<endl;
+//     cout<<id<<endl;
+//     cout<<favSong<<endl;
+//     cout<<favMovie<<endl;
+//
+// }
+
+
+//problem 3
+// int main() {
+//     string poem = "\"I'm standing\" on the edge of some crazy cliff.\n"
+//                   "What I have to do, I have to catch evrybody\\\n"
+//                   "if they start to go over the cliff-I mean\\\n"
+//                   "\\if they're running and they don't look\n"
+//                   "where they're going I have to come out\n"
+//                   "from somewhere and \t catch them.";
+//     cout << poem << endl;
+// }
+
+
+// problem 4
+// int main() {
+//     int a = 189;
+//     char b = 'B';
+//     float floatNumber = a/3;
+//     int decimalValue = b;
+//     float floatNumber2 = (a+b)/5;
+//     char c = b + a/10 - 1;
+//     cout<<floatNumber<<"\t"<<decimalValue<<"\t"<<floatNumber2<<"\t"<<c<<endl;
+// }
+
+
+// problem 5
 int main() {
-    int intNumber = 24;
-    float floatNumber = 3.14;
-    double doubleNumber = 45.1234;
-    bool boolean = true;
-    char charName = 'A';
-    cout <<"value of integer number is: "<<intNumber<<" Size of it is "<< sizeof(intNumber)<< endl;
-    cout <<"Value of integer number is: "<<doubleNumber<<" Size of it is "<< sizeof(doubleNumber)<< endl;
-    cout <<"Value of integer number is: "<<boolean<<" Size of it is "<< sizeof(boolean)<<endl;
-    cout <<"Value of integer number is: "<<charName<<" Size of it is "<< sizeof(charName)<<endl;
+    int a, b;
+    cin >> a >> b;
+    int sum = a + b;
+    int product = a * b;
+    cout <<"Sum = "<<sum<< endl;
+    cout <<"Product = "<<product<< endl;
 }
-
